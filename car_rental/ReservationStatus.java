d0f8211ac914281ee1ec94fdd906ca5d79688cce
Sailesh Panda <saileshpanda1010@gmail.com>
2025-07-01 20:21:26 +0530

    Car_rental_1

---
+++ car_rental/ReservationStatus.java
@@ -0,0 +1,9 @@
+
+
+public enum ReservationStatus {
+
+    SCHEDULED,
+    INPROGRESS,
+    COMPLETED,
+    CANCELLED;
+}
---
+++ car_rental/ReservationType.java
@@ -0,0 +1,7 @@
+
+
+public enum ReservationType {
+
+    HOURLY,
+    DAILY;
+}
